6078298102d1fd9c9461b2375e5fc461504a6d59
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-07 17:23:05 +0200

    54-1 Module Introduction, simple react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container mx-auto p-2 md:p-4">
+        <h3>Hello</h3>
+      </div>
+    </div>
+  );
 };
 
 export default App;
